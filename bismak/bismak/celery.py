2214cbb6dff9b181cb7b1179ccca801a2451a0c1
[Abdulazeez Idowu] <[idowuabdulazeez39@gmail.com]>
2026-06-13 01:14:09 +0100

    fix(celery): default to prod settings module

--- bismak/bismak/celery.py
+++ bismak/bismak/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bismak.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bismak.settings.prod')
 
 app = Celery('bismak')
 app.config_from_object('django.conf:settings', namespace='CELERY')
